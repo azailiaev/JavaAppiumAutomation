a1858903880303aec97ea5afb36336e53279ac52
azailiaev <zailyaev_artur@mail.ru>
2023-04-05 21:03:13 +0200

    Refactoring

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -10,22 +10,18 @@ import java.net.URL;
 import java.time.Duration;
 
 public class CoreTestCase extends TestCase {
+    private static final String PLATFORM_IOS = "ios";
+    private static final String PLATFORM_ANDROID = "android";
+
     protected AppiumDriver driver;
-    private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
+    private static String AppiumURL = "http://127.0.0.1:4723";
 
     @Override
     protected void setUp() throws Exception
     {
         super.setUp();
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName","Android");
-        capabilities.setCapability("deviceName","and10");
-        capabilities.setCapability("platformVersion","10");
-        capabilities.setCapability("automationName","Appium");
-        capabilities.setCapability("appPackage","org.wikipedia");
-        capabilities.setCapability("appActivity",".main.MainActivity");
-        capabilities.setCapability("app","C:\\Users\\zaily\\OneDrive\\Документы\\GitHub\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
+        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
         driver = new AndroidDriver(new URL(AppiumURL), capabilities);
         this.rotateScreenPortrait();
     }
@@ -51,5 +47,34 @@ public class CoreTestCase extends TestCase {
     protected void backgroundApp(int seconds)
     {
         driver.runAppInBackground(Duration.ofSeconds(seconds));
+    }
+
+    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception {
+        String platform = System.getenv("PLATFORM");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        if (platform.equals(PLATFORM_ANDROID)) {
+            capabilities.setCapability("platformName", "Android");
+            capabilities.setCapability("deviceName", "and10");
+            capabilities.setCapability("platformVersion", "10");
+            capabilities.setCapability("automationName", "Appium");
+            capabilities.setCapability("appPackage", "org.wikipedia");
+            capabilities.setCapability("appActivity", ".main.MainActivity");
+            capabilities.setCapability("app", "/Users/marialarina/Documents/GitHub/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        } else if (platform.equals(PLATFORM_IOS)) {
+
+            capabilities.setCapability("platformName", "iOS");
+            capabilities.setCapability("appium:deviceName", "iPhone 14");
+            capabilities.setCapability("appium:platformVersion", "16.0");
+            capabilities.setCapability("appium:automationName", "XCUITest");
+            capabilities.setCapability("appium:app", "/Users/marialarina/Documents/GitHub/JavaAppiumAutomation/apks/Wikipedia.app");
+
+        } else
+        {
+            throw new Exception("Cannot get run platform from env variable. Platform value " + platform);
+        }
+
+        return capabilities;
     }
 }
